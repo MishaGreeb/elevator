b6f2401c4040c0fec4f672b5fa5790f7ed39a75a
Tolyan <toxamash@gmail.com>
2017-11-14 23:41:17 +0200

    add new method for mtd

--- elevator.py
+++ elevator.py
@@ -1,4 +1,5 @@
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from scp import SCPClient, SCPException
 import argparse
 
@@ -6,9 +7,9 @@ import traceback
 import paramiko
 import sys
 import os
-
+import socket
+import time
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 
 ######################
 # Setup              #
@@ -28,6 +29,32 @@ options = parser.parse_args()
 
 LOGGING = options.verbose
 
+def fwinfo(txt):
+    txt = txt.replace('.', '')
+    typefirmware = txt[:2]
+    verfirmware = int(txt[3:6])
+    if verfirmware > 604:
+        firmware_signed = True
+    else:
+        firmware_signed = False
+    return typefirmware, firmware_signed
+
+def extract_files(file):
+    from subprocess import call,check_output
+    from platform import system
+    os = system()
+    if os == 'Linux':
+        call(['./fwsplit', file], shell = True)
+    elif os == 'Windows':
+        line = check_output(['fwsplit.exe',file], shell = True)
+    lines = line.splitlines()
+    for line in lines:
+        if ".kernel" in line:
+            firmware_kernel = line.strip("\t\n ")
+        if ".rootfs" in line:
+            firmware_rootfs = line.strip("\t\n ")
+    #print "Kernel: %s; Rotfs: %s" % (kernel, rootfs)
+    return firmware_kernel, firmware_rootfs
 
 ######################
 # Options validation #
@@ -38,7 +65,6 @@ if not os.path.isfile(os.path.join(current_dir, options.template)):
 if options.update_firmware:
     if not os.path.isfile(os.path.join(current_dir, options.update_firmware)):
         parser.error("Firmware '%s' not found" % options.update_firmware)
-
 
 ######################
 # Connect to device  #
@@ -61,11 +87,10 @@ try:
         hostname=host, username=username,
         password=password, port=port
     )
-except paramiko.ssh_exception.AuthenticationException as error:
+except (paramiko.ssh_exception.AuthenticationException,socket.error) as error:
     tb = traceback.format_exc()
-    print(error)
+    print (error)
     sys.exit()
-
 
 ######################
 # Main  #
@@ -75,12 +100,14 @@ scp = SCPClient(client.get_transport())
 conf_name = ".configured_%s" % options.firmware_version
 
 try:
+    stdin, stdout, stderr = client.exec_command('cat /etc/version')
+    data = stdout.read() + stderr.read()
+    fwtype, fwsign = fwinfo(data)
     stdin, stdout, stderr = client.exec_command("mkdir -p /etc/persistent/mnt/config/")
 
     scp.put(options.template, "/etc/persistent/mnt/config/%s" % conf_name)
     if LOGGING:
         print("configiguration file copied")
-
 
     scp.put(os.path.join(current_dir, "passwd"), "/etc/persistent/mnt/config/passwd")
     if LOGGING:
@@ -94,17 +121,31 @@ try:
         print("configuration files saved to flash")
 
     if options.update_firmware:
-        scp.put(options.update_firmware, "/tmp/fwupdate.bin")
-        if LOGGING:
-            print("Firmware copied")
-        stdin, stdout, stderr = client.exec_command("/usr/bin/fwupdate -m /tmp/fwupdate.bin")
-        if LOGGING:
+        if fwsign:
+            kernel,rootfs = extract_files(options.update_firmware)
+            print("Firmware copy to %s" %host)
+            scp.put("mtd", "/tmp/mtd")
+            scp.put(kernel, "/tmp/kernel")
+            scp.put(rootfs, "/tmp/rootfs")
+            #set attribute
+            stdin, stdout, stderr = client.exec_command('chmod +x /tmp/mtd;chmod +x /tmp/kernel;chmod +x /tmp/rootfs;')
             print("Firmware update started")
-except (SCPException,paramiko.SSHException)  as error:
+            time.sleep(2)
+            stdin, stdout, stderr = client.exec_command('/tmp/mtd write /tmp/kernel kernel && /tmp/mtd -r write /tmp/rootfs rootfs')
+            for line in stderr:
+                print(line)
+                if line.count("Rebooting") > 0:
+                    print("Firmware %s\t of %s successfully completed" % (options.update_firmware, host))
+                    break
+        else:
+            scp.put(options.update_firmware, "/tmp/fwupdate.bin")
+            print("Firmware update started")
+            stdin, stdout, stderr = client.exec_command("/usr/bin/fwupdate -m /tmp/fwupdate.bin")
+            if LOGGING:
+                print("Elevation of %s successfully completed" % host)
+except (SCPException, paramiko.SSHException) as error:
     tb = traceback.format_exc()
     print(error)
     sys.exit()
 
 client.close()
-
-print("Elevation of %s successfully completed" % host)
